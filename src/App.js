f91df1efae68d3258844de88b870dfcf00f41f88
adithya-tp <adithyatp@yahoo.com>
2021-06-18 16:24:28 +0400

    get ids first draft

--- src/App.js
+++ src/App.js
@@ -15,6 +15,7 @@ function App() {
   const [description, setDescription] = useState('');
   const [syncons, setSyncons] = useState({});
   const [knldge, setKnldge] = useState({});
+  const [positions, setPositions] = useState([]);
 
   useEffect(() => {
       db.collection('text')
@@ -23,7 +24,9 @@ function App() {
           setDescription(snapshot.data().current)
       });
       console.log("Description: ", description);
-  });
+  }, [positions, description]);
+
+  var tpToken = 'eyJraWQiOiI1RDVOdFM1UHJBajVlSlVOK1RraXVEZE15WWVMMFJQZ3RaUDJGTlhESHpzPSIsImFsZyI6IlJTMjU2In0.eyJjdXN0b206Y291bnRyeSI6IkFFIiwic3ViIjoiZTMxMjAyMmEtNzI4MC00ZGJmLWFlYmUtOGFiNDNjZWQ5NzQ3IiwiZW1haWxfdmVyaWZpZWQiOnRydWUsImN1c3RvbTpwZXJzb25hbGl6YXRpb25BdXRoIjoiMCIsImlzcyI6Imh0dHBzOlwvXC9jb2duaXRvLWlkcC5ldS13ZXN0LTEuYW1hem9uYXdzLmNvbVwvZXUtd2VzdC0xX0FVSGdRMDhDQiIsInBob25lX251bWJlcl92ZXJpZmllZCI6ZmFsc2UsImNvZ25pdG86dXNlcm5hbWUiOiJlMzEyMDIyYS03MjgwLTRkYmYtYWViZS04YWI0M2NlZDk3NDciLCJjdXN0b206Y29tcGFueSI6IlN0dWRlbnQiLCJhdWQiOiIxZWdzNjNxOTlwM3NlYmVjaHNiNzI5dDgwbyIsImV2ZW50X2lkIjoiZjA3NGRlNzgtOWUxNi00NzcyLWIwMDctM2UxZDdhNjA4NjFiIiwidG9rZW5fdXNlIjoiaWQiLCJhdXRoX3RpbWUiOjE2MjM5NTA0ODEsIm5hbWUiOiJBZGl0aHlhIiwiZXhwIjoxNjI0MDM2ODgxLCJpYXQiOjE2MjM5NTA0ODEsImZhbWlseV9uYW1lIjoiUHJhdmVlbiIsImVtYWlsIjoiZjIwMTcwMTk5QGR1YmFpLmJpdHMtcGlsYW5pLmFjLmluIiwiY3VzdG9tOm1hcmtldGluZ0F1dGgiOiIwIn0.BBTOFyADjeLpCNeQkPxcweu-VcVvkSACKdXet3Wrk25pLCuSmWrYoMH-OKXlZMW-B3LhHz5JrPNCyd603XpKd6ItA02iqL-WZR_hxm5bc_DWhX9CzJwehZpqyIPxGptExx0T8hyTSX6sInO39QrAHgQvi2N-UtAIQSyD0QRNgVVHEf_9vXO_vU91C7K8F70bx7lS-JlH55clyO9_fCJcxZ1P7BDX1gYvMp938b0PlQHZmWrPOkW406W4gSZOTiwLwS4dhzDnK0ttacgAZefLsrZcoaBoH9gyVcnP5q7zpJFhon5LRAAksMxDeFm5T9D5i031CB9VDH1Oq669b7Y5TQ';
 
   const getSyncons = () => {
     axios({
@@ -31,7 +34,7 @@ function App() {
       url: 'https://nlapi.expert.ai/v2/analyze/standard/en/relevants',
       headers: {
         'Content-Type': 'application/json; charset=utf-8',
-        'Authorization': 'Bearer eyJraWQiOiI1RDVOdFM1UHJBajVlSlVOK1RraXVEZE15WWVMMFJQZ3RaUDJGTlhESHpzPSIsImFsZyI6IlJTMjU2In0.eyJjdXN0b206Y291bnRyeSI6IkFFIiwic3ViIjoiZTMxMjAyMmEtNzI4MC00ZGJmLWFlYmUtOGFiNDNjZWQ5NzQ3IiwiZW1haWxfdmVyaWZpZWQiOnRydWUsImN1c3RvbTpwZXJzb25hbGl6YXRpb25BdXRoIjoiMCIsImlzcyI6Imh0dHBzOlwvXC9jb2duaXRvLWlkcC5ldS13ZXN0LTEuYW1hem9uYXdzLmNvbVwvZXUtd2VzdC0xX0FVSGdRMDhDQiIsInBob25lX251bWJlcl92ZXJpZmllZCI6ZmFsc2UsImNvZ25pdG86dXNlcm5hbWUiOiJlMzEyMDIyYS03MjgwLTRkYmYtYWViZS04YWI0M2NlZDk3NDciLCJjdXN0b206Y29tcGFueSI6IlN0dWRlbnQiLCJhdWQiOiIxZWdzNjNxOTlwM3NlYmVjaHNiNzI5dDgwbyIsImV2ZW50X2lkIjoiZmQzOWIwZWYtN2FkMC00MjQ5LWIzZDgtYjFmM2UxYzUwYTAzIiwidG9rZW5fdXNlIjoiaWQiLCJhdXRoX3RpbWUiOjE2MjM3ODAzMDIsIm5hbWUiOiJBZGl0aHlhIiwiZXhwIjoxNjIzODY2NzAyLCJpYXQiOjE2MjM3ODAzMDIsImZhbWlseV9uYW1lIjoiUHJhdmVlbiIsImVtYWlsIjoiZjIwMTcwMTk5QGR1YmFpLmJpdHMtcGlsYW5pLmFjLmluIiwiY3VzdG9tOm1hcmtldGluZ0F1dGgiOiIwIn0.ccni516VsHXDmpEhQTlP63i6YgcSyvaVH8IrzivVCOGEud6tiWg4hcLNqD1__QcI_VE1vzGzdZW_ja6RhoTwKuavEuR-fBB1SdHpnalI7bO_cYj7FWesyupLPZdlXp8zYcAiPxEniBmcC2gX384vJ7JdpASKOJYQgOEA-vuDbf0uyU7Mjkx88BBDuJjnyN7VpIAJI2vAaL5g1VhSoRn7QqEfq3KubRaeLiwIfJtoWk4Zr3j2shhMd9FpwHgCvzjcS7fRyfRnvlDl2IhkqK8rkM3xxDnhfZ3wKk-vHe2PMRqyPNrrJyAXCllxEAozx4rq0RdHD3DNALdlNIh8gGrNxw'
+        'Authorization': `Bearer ${tpToken}`
       },
       data: {
         document: {
@@ -42,8 +45,29 @@ function App() {
     .then(function (response) {
       setSyncons(response.data.data.mainSyncons);
       setKnldge(response.data.data.knowledge);
-      console.log(response);
+      getIDs(response.data.data.knowledge, response.data.data.mainSyncons);
     });
+  }
+
+  function getIDs(respK, respS) {
+    const diseaseLabels = new Set(['illness.disease', 'disease.pathological_process', 'process.biological_process']);
+    var synconLabelDict = new Object();
+
+    console.log("NAWWWLEJJJJJ", knldge);
+    respK.forEach(item => {
+      synconLabelDict[item["syncon"]] = item["label"];
+    });
+
+    var pos = [];
+    respS.forEach(item => {
+      if(diseaseLabels.has(synconLabelDict[item["syncon"]])) {
+        item["positions"].forEach(posarr => {
+          pos.push([posarr["start"], posarr["end"]]);
+        })
+      }
+    });
+    setPositions(pos);
+    console.log("Posishhh: ", pos);
   }
 
   return (
@@ -51,7 +75,7 @@ function App() {
       <div className="main__container">
         <div className="top__container">
           <div className="left__side">
-            <PatientDescription />
+            <PatientDescription positions={positions} />
             <h3 style={{ fontSize: "25px", textAlign: "left", marginLeft: "15vw", fontFamily: "Arvo"}}>Medical Code Options</h3>
             <div className="coding__btns">
               <GradientButton buttonClassName="icd__btn" color="#8865ff" textColor="#ffffff" text="ICD-10"/>
--- src/components/PatientDescription/PatientDescription.js
+++ src/components/PatientDescription/PatientDescription.js
@@ -4,7 +4,8 @@ import { Button } from '@progress/kendo-react-buttons';
 import { db } from '../../firebase';
 import './PatientDescription.css';
 
-const PatientDescription = () => {
+const PatientDescription = ({ positions }) => {
+    console.log(positions);
     const [description, setDescription] = useState('');
 
     useEffect(() => {
@@ -15,14 +16,28 @@ const PatientDescription = () => {
         });
     }, []);
 
+    function highlightWords(pos) {
+        var desCopy = description;
+        pos.forEach(item => {
+            var subs = `<span class='highlight__text'>${desCopy.substring(item[0], item[1] + 1)}</span>`
+            setDescription(desCopy.substring(0, item[0]) + subs + desCopy.substring(item[1]));
+        });
+    }
+
     return (
         <div className="textarea__component">
+            {/* {
+                highlightWords(positions)
+            } */}
             <Button className="desc__button">
                 <Label>
                     {'Patient Medical Description'}
                 </Label>
             </Button>
-            <textarea name='medicalDescription' id='medicalDescription' cols="30" rows="12" value={description} onChange={(e) => setDescription(e.target.value)}>
+            <textarea name='medicalDescription' id='medicalDescription' cols="30" rows="12" value={description} onChange={(e) => {
+                positions=[];
+                setDescription(e.target.value)
+            }}>
 
             </textarea>
         </div>
